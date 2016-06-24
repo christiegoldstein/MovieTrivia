c241fd7a0087b52b752c9e01c8cecafe24c4f5ba
christiegoldstein <cngold15@gmail.com>
2016-06-24 14:50:32 -0400

    divided movies array into arrays of 20 then took the first index

--- app.rb
+++ app.rb
@@ -1,5 +1,23 @@
+require_relative("lib/movie.rb")
 require("sinatra")
 require("imdb")
 
 
+movie = Movie.new
+
+get "/" do
+	erb(:home)
+end
+
+post "/movie_list" do
+	@term = params[:term]
+
+	the_search = Imdb::Search.new(@term)
+
+	all_movies = the_search.movies.each_slice(20).to_a
+
+	@movies = all_movies[0]
+
+	erb(:poster)
+end
 
